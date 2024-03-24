499e739ceca91627696640b75d42d9ff4322208b
YakovShonin <yakov.shonin@gmail.com>
2024-03-24 12:40:12 +0100

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,22 +1,23 @@
-const form = document.querySelector(`.feedback-from`);
+const form = document.querySelector(".feedback-from");
 const email = form.elements.email;
 const message = form.elements.message;
+const key = "feedback-form-state";
 
-if (localStorage.getItem(key)) {
-    setPreviousValue();
-}
 
-form.addEventListener(`input`, saveValue);
+form.addEventListener("input", saveValue);
 function saveValue(event) {
-    const valueObj = JSON.parse(localStorage.getItem(`feedback-form-state`)) || {};
+    const valueObj = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
     const value = event.target.value;
     valueObj[event.target.name] = value.trim();
     localStorage.setItem(key, JSON.stringify(valueObj));
 }
+if (localStorage.getItem(key)) {
+    setPreviousValue();
+}
 
 function setPreviousValue() {
     const previousValues = JSON.parse(
-        localStorage.getItem(`feedback-form-state`)
+        localStorage.getItem("feedback-form-state")
     );
     const previousEmail = previousValues.email;
     const previousMessage = previousValues.message;
@@ -24,14 +25,14 @@ function setPreviousValue() {
     message.value = previousMessage || ``;
 }
 
-form.addEventListener(`submit`, formSubmit);
+form.addEventListener("submit", formSubmit);
 function formSubmit(event) {
     if (email.value.trim() && message.value.trim()) {
         event.preventDefault();
-        console.log(JSON.parse(localStorage.getItem(`feedback-form-state`)));
+        console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
         event.currentTarget.reset();
-        localStorage.removeItem(`feedback-form-state`);
+        localStorage.removeItem("feedback-form-state");
     } else {
-        alert(`Fill in the form`)
+        alert("Fill in all the fields of the form")
     }
 }
