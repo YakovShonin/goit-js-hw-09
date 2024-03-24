6781ea702680545ad9b08d7e8ca3e872e375136d
YakovShonin <yakov.shonin@gmail.com>
2024-03-24 21:04:35 +0100

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,38 +1,32 @@
-const form = document.querySelector(".feedback-from");
+const form = document.querySelector('.feedback-form');
 const email = form.elements.email;
 const message = form.elements.message;
-const key = "feedback-form-state";
-
-
-form.addEventListener("input", saveValue);
-function saveValue(event) {
-    const valueObj = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
-    const value = event.target.value;
-    valueObj[event.target.name] = value.trim();
-    localStorage.setItem(key, JSON.stringify(valueObj));
-}
+const key = 'feedback-form-state';
 if (localStorage.getItem(key)) {
-    setPreviousValue();
+  setPreviousValue();
 }
-
+form.addEventListener('input', saveValue);
+function saveValue(event) {
+  const valueObj = JSON.parse(localStorage.getItem(key)) || {};
+  const value = event.target.value;
+  valueObj[event.target.name] = value.trim();
+  localStorage.setItem(key, JSON.stringify(valueObj));
+}
 function setPreviousValue() {
-    const previousValues = JSON.parse(
-        localStorage.getItem("feedback-form-state")
-    );
-    const previousEmail = previousValues.email;
-    const previousMessage = previousValues.message;
-    email.value = previousEmail || ``;
-    message.value = previousMessage || ``;
+  const previousValues = JSON.parse(localStorage.getItem(key));
+  const previousEmail = previousValues.email;
+  const previousMessage = previousValues.message;
+  email.value = previousEmail || '';
+  message.value = previousMessage || '';
 }
-
-form.addEventListener("submit", formSubmit);
+form.addEventListener('submit', formSubmit);
 function formSubmit(event) {
-    if (email.value.trim() && message.value.trim()) {
-        event.preventDefault();
-        console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
-        event.currentTarget.reset();
-        localStorage.removeItem("feedback-form-state");
-    } else {
-        alert("Fill in all the fields of the form")
-    }
+  if (email.value.trim() && message.value.trim()) {
+    event.preventDefault();
+    console.log(JSON.parse(localStorage.getItem(key)));
+    event.currentTarget.reset();
+    localStorage.removeItem(key);
+  } else {
+    alert('You should fill in the form');
+  }
 }
